9e4ca6e19b0020d4380628b5e3c70dd905307fa4
Heeryun, Jin(qwertycvb) <jins4218@gmail.com>
2024-09-21 19:59:04 +0900

    feat: SUIT 폰트 추가
    Signed-off-by: Heeryun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ lib/app/config/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData appTheme = ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      titleSpacing: 0,
+    ),
+    fontFamily: 'SUIT',
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
