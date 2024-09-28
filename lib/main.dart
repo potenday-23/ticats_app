f3e5a75f87cf529e1c877b6f32e7dbeb0c1b61f0
Heeryun, Jin(qwertycvb) <jins4218@gmail.com>
2024-09-28 18:40:34 +0900

    feat: Kakao 로그인 추가
    Signed-off-by: Heeryun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'package:talker_riverpod_logger/talker_riverpod_logger.dart';
 import 'package:ticats_app/app/config/app_router.dart';
@@ -12,6 +13,9 @@ void main() {
 
   // Initialize talker
   final talker = TalkerFlutter.init();
+
+  // Initialize kakao sdk
+  KakaoSdk.init(nativeAppKey: "e431334bbd653b087fa3bdc1d431c42e");
 
   runApp(
     ProviderScope(
