4120e58ca55784b56b427e6310d68aed062f98ec
chojja7188 <shoun7188@naver.com>
2024-10-05 17:40:33 +0900

    feat: 카테고리 리스트 페이지 라우팅

--- lib/presentation/home/view/home_category_view.dart
+++ lib/presentation/home/view/home_category_view.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/src/consumer.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ticats_app/app/base/base_view.dart';
+import 'package:ticats_app/app/config/app_router.dart';
 import 'package:ticats_app/app/config/app_typeface.dart';
 import 'package:ticats_app/app/enum/ticats_event_category.enum.dart';
 import 'package:ticats_app/presentation/common/widget/ticats_chip.dart';
 
-class HomeCategoryView extends StatelessWidget {
+class HomeCategoryView extends BaseView {
   const HomeCategoryView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget buildView(BuildContext context, WidgetRef ref) {
     return SingleChildScrollView(
       physics: const ClampingScrollPhysics(),
       scrollDirection: Axis.horizontal,
@@ -18,7 +21,16 @@ class HomeCategoryView extends StatelessWidget {
           for (final category in TicatsEventCategory.values)
             Padding(
               padding: EdgeInsets.only(right: 8.w),
-              child: TicatsChip(category.label, onTap: () {}, textStyle: AppTypeface.label14Bold),
+              child: TicatsChip(
+                  category.label,
+                  onTap: () {
+                    ref.read(routerProvider).pushNamed(
+                        Routes.eventListOfCategory,
+                        queryParameters: {'category': category.label},
+                    );
+                  },
+                  textStyle: AppTypeface.label14Bold
+              ),
             ),
           SizedBox(width: 18.w),
         ],
