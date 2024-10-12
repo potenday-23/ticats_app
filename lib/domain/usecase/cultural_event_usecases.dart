50c53b9382f1c476dc40a9e4c2c510dfe80c4b4d
chojja7188 <shoun7188@naver.com>
2024-10-13 03:24:23 +0900

    feat: 카테고리 리스트 페이지 초안 작성

--- lib/domain/usecase/cultural_event_usecases.dart
+++ lib/domain/usecase/cultural_event_usecases.dart
@@ -12,6 +12,7 @@ class CulturalEventUsecases {
   final CulturalEventRepository _repository;
 
   GetEventInfo get getEventInfo => GetEventInfo(_repository);
+  GetEvents get getEvents => GetEvents(_repository);
   GetOpenDateEvents get getOpenDateEvents => GetOpenDateEvents(_repository);
   GetPointEvents get getPointEvents => GetPointEvents(_repository);
   GetRecommendEvents get getRecommendEvents => GetRecommendEvents(_repository);
@@ -24,6 +25,16 @@ class GetEventInfo {
 
   Future<CulturalEventEntity> execute(String id) async {
     return await _repository.getCulturalEventInfo(id);
+  }
+}
+
+class GetEvents {
+  final CulturalEventRepository _repository;
+
+  GetEvents(this._repository);
+
+  Future<List<CulturalEventEntity>> execute(CulturalEventsSearchEntity request) async {
+    return await _repository.getCulturalEvents(request);
   }
 }
 
--- lib/presentation/event_list/event_list_of_category_page.dart
+++ lib/presentation/event_list/event_list_of_category_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/src/consumer.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ticats_app/app/base/base_page.dart';
 import 'package:ticats_app/presentation/common/app_bar/ticats_app_bar.dart';
+import 'package:ticats_app/presentation/event_list/view/event_list_event_view.dart';
+import 'package:ticats_app/presentation/event_list/view/event_list_filter_view.dart';
 
 class EventListOfCategory extends BasePage {
   const EventListOfCategory({required this.categoryName, super.key});
@@ -13,8 +15,12 @@ class EventListOfCategory extends BasePage {
   Widget buildPage(BuildContext context, WidgetRef ref) {
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: 20.h),
+          const EventListFilterView(),
+          SizedBox(height: 40.h),
+          EventListEventView(categoryName)
         ],
       ),
     );
