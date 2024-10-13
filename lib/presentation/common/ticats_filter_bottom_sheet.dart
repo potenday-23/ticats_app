ed465a766656c16db76f53411df3b53d11484444
chojja7188 <shoun7188@naver.com>
2024-10-13 23:11:29 +0900

    feat: 기본적인 바텀시트 구현 및 필터 작업 임시 커밋

---
+++ lib/presentation/common/ticats_filter_bottom_sheet.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ticats_app/app/config/app_router.dart';
+import 'package:ticats_app/app/config/app_typeface.dart';
+import 'package:ticats_app/app/enum/ticats_event_category.enum.dart';
+import 'package:ticats_app/app/enum/ticats_event_ordering.enum.dart';
+
+Future<void> showTicatFilterBottomSheet<T>({
+  required List<T> options,
+  required T groupValue,
+  required ValueChanged<T> onChanged,
+}) async {
+  final context = rootNavigatorKey.currentContext;
+
+  if (context != null) {
+    showModalBottomSheet(
+        context: context,
+        elevation: 0,
+        isScrollControlled: false,
+        isDismissible: true,
+        builder: (context) {
+          return TicatsFilterBottomSheet(
+            options: options,
+            groupValue: groupValue,
+            onChanged: onChanged
+          );
+        }
+    );
+  }
+}
+
+class TicatsFilterBottomSheet<T> extends StatelessWidget {
+  final List<T> options;
+  final T groupValue;
+  final ValueChanged<T> onChanged;
+
+  const TicatsFilterBottomSheet({
+    super.key,
+    required this.options,
+    required this.groupValue,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Container(
+        padding: EdgeInsets.only(left: 35.w, top: 24.h, right: 35.w, bottom: 58.h),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text('정렬', style: AppTypeface.body20Bold, textAlign: TextAlign.center),
+            SizedBox(height: 32.h),
+            ...options.map((option) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 320.w,
+                    child: RadioListTile<T>(
+                      title: Text(_getEnumLabel(option)),
+                      value: option,
+                      groupValue: groupValue,
+                      onChanged: (value) {
+                        if (value != null) {
+                          onChanged(value);
+                          // context.pop();
+                        }
+                      },
+                      controlAffinity: ListTileControlAffinity.trailing,
+                    ),
+                  )
+                ],
+              );
+            }),
+          ],
+        ),
+      ),
+    ]);
+  }
+
+  String _getEnumLabel<T>(T value) {
+    if (value is TicatsEventOrdering) {
+      return value.label;
+    }
+    return '';
+  }
+}
--- lib/presentation/event_list/view/event_list_filter_view.dart
+++ lib/presentation/event_list/view/event_list_filter_view.dart
@@ -3,13 +3,19 @@ import 'package:flutter_riverpod/src/consumer.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ticats_app/app/base/base_view.dart';
 import 'package:ticats_app/app/config/app_typeface.dart';
+import 'package:ticats_app/app/enum/ticats_event_ordering.enum.dart';
+import 'package:ticats_app/domain/entity/cultural_event/cultural_events_search_entity.dart';
+import 'package:ticats_app/presentation/common/ticats_filter_bottom_sheet.dart';
 import 'package:ticats_app/presentation/common/widget/ticats_chip.dart';
+import 'package:ticats_app/presentation/event_list/provider/event_list_controller.dart';
 
 class EventListFilterView extends BaseView {
   const EventListFilterView({super.key});
 
   @override
   Widget buildView(BuildContext context, WidgetRef ref) {
+    final filter = ref.watch(eventListControllerProvider()).value?.filter
+        ?? const CulturalEventsSearchEntity();
     return SingleChildScrollView(
       physics: const ClampingScrollPhysics(),
       scrollDirection: Axis.horizontal,
@@ -18,33 +24,12 @@ class EventListFilterView extends BaseView {
         children: [
           SizedBox(width: 20.w),
           TicatsChip.arrowDown('추천순', onTap: () {
-            /*
-            showModalBottomSheet(
-                context: context,
-                elevation: 0,
-                builder: (context) {
-                  return Container(
-                    padding: EdgeInsets.only(top: 24.h),
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Text('정렬', style: AppTypeface.body20Bold, textAlign: TextAlign.center),
-                        SizedBox(height: 32.h),
-                        Row(
-                          children: [
-                            Text('기본순'),
-                            RadioListTile(
-                                value: '기본순',
-                                groupValue: 'ㄴㅇㄹ',
-                                onChanged: (value) {}
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-            });
-             */
+            showTicatFilterBottomSheet(
+                options: TicatsEventOrdering.values,
+                groupValue: TicatsEventOrdering.recommend,
+                onChanged: (value) {
+                  ref.read(eventListControllerProvider().notifier).selectOrdering(value);
+                });
           }),
           SizedBox(width: 8.w),
           TicatsChip.arrowDown('공연 현황', onTap: () {
