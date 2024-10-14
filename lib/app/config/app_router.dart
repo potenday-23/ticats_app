fe2a6bd11a9049875372626bc1b23c146b3929c2
chojja7188 <shoun7188@naver.com>
2024-10-14 23:28:49 +0900

    feat: 이벤트 상세 페이지 추가 임시커밋

--- lib/app/config/app_router.dart
+++ lib/app/config/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:ticats_app/presentation/event_detail/event_detail_page.dart';
 import 'package:ticats_app/presentation/event_list/event_list_of_category_page.dart';
 import 'package:ticats_app/presentation/login/login_page.dart';
 import 'package:ticats_app/presentation/main/main_page.dart';
@@ -20,6 +21,9 @@ class Routes {
 
   // Event List
   static const String eventListOfCategory = '/eventList/category';
+
+  // Event Detail
+  static const String eventDetail = '/eventDetail';
 
   // Register
   static const String registerProfile = 'profile';
@@ -55,6 +59,13 @@ class Router extends _$Router {
           path: Routes.eventListOfCategory,
           name: Routes.eventListOfCategory,
           builder: (context, state) => EventListOfCategoryPage(categoryName: state.uri.queryParameters['category'] as String),
+        ),
+
+        // Event Detail
+        GoRoute(
+          path: Routes.eventDetail,
+          name: Routes.eventDetail,
+          builder: (context, state) => EventDetailPage(),
         ),
 
         // Register
