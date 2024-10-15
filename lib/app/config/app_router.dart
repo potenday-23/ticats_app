c7c6348bc027ebc6e1a91d471d743e1f02a27bd5
chojja7188 <shoun7188@naver.com>
2024-10-15 23:32:30 +0900

    feat: 이벤트 상세 페이지 provider 생성

--- lib/app/config/app_router.dart
+++ lib/app/config/app_router.dart
@@ -65,7 +65,7 @@ class Router extends _$Router {
         GoRoute(
           path: Routes.eventDetail,
           name: Routes.eventDetail,
-          builder: (context, state) => EventDetailPage(),
+          builder: (context, state) => EventDetailPage(int.parse(state.uri.queryParameters['id']!)),
         ),
 
         // Register
