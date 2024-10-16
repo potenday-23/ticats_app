6c46cdfe780f2c849fdb5cd4989cfb87e4db6146
chojja7188 <shoun7188@naver.com>
2024-10-17 04:52:14 +0900

    refactor: import 수정

--- lib/presentation/event_detail/event_detail_page.dart
+++ lib/presentation/event_detail/event_detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_riverpod/src/consumer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ticats_app/app/base/base_page.dart';
 import 'package:ticats_app/app/config/app_color.dart';
