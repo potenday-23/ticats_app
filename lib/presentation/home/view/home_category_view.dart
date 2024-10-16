276b7c044390fba9524d05cf0d98021e6dfd8ab2
chojja7188 <shoun7188@naver.com>
2024-10-17 04:52:14 +0900

    refactor: import 수정

--- lib/presentation/home/view/home_category_view.dart
+++ lib/presentation/home/view/home_category_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/src/consumer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ticats_app/app/base/base_view.dart';
 import 'package:ticats_app/app/config/app_router.dart';
