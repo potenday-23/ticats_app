0cbe2c5b7cde046e14cce47d9caa0a8d6a3f228c
chojja7188 <shoun7188@naver.com>
2024-10-17 14:54:10 +0900

    style: 필터 바텀시트 디자인 수정

--- lib/presentation/common/ticats_filter_bottom_sheet.dart
+++ lib/presentation/common/ticats_filter_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ticats_app/app/config/app_color.dart';
 import 'package:ticats_app/app/config/app_router.dart';
 import 'package:ticats_app/app/config/app_typeface.dart';
 import 'package:ticats_app/app/enum/ticats_event_ordering.enum.dart';
@@ -14,6 +15,7 @@ Future<void> showTicatFilterBottomSheet<T>({
   if (context != null) {
     showModalBottomSheet(
         context: context,
+        backgroundColor: AppColor.white,
         elevation: 0,
         isScrollControlled: false,
         isDismissible: true,
@@ -64,25 +66,38 @@ class _TicatsFilterBottomSheetState<T> extends State<TicatsFilterBottomSheet<T>>
               Text('정렬', style: AppTypeface.body20Bold, textAlign: TextAlign.center),
               SizedBox(height: 32.h),
               ...widget.options.map((option) {
+                bool isSelected = groupValue == option;
                 return Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     SizedBox(
                       width: 320.w,
-                      child: RadioListTile<T>(
-                        title: Text(_getEnumLabel(option)),
-                        value: option,
-                        groupValue: groupValue,
-                        onChanged: (value) {
-                          if (value != null) {
-                            setState(() {
-                              groupValue = value;
-                            });
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                            radioTheme: RadioThemeData(
+                              fillColor: WidgetStateProperty.resolveWith<Color>((states) {
+                                return AppGrayscale.gray50; // 선택되지 않은 상태의 색상
+                              }),
+                            )
+                        ),
+                        child: RadioListTile<T>(
+                          contentPadding: EdgeInsets.zero,
+                          visualDensity: VisualDensity(vertical: -4.h),
+                          activeColor: AppColor.primaryNormal,
+                          title: Text(_getEnumLabel(option), style: AppTypeface.body18Semibold.copyWith(color: isSelected ? AppColor.primaryDark : AppGrayscale.gray50)),
+                          value: option,
+                          groupValue: groupValue,
+                          onChanged: (value) {
+                            if (value != null) {
+                              setState(() {
+                                groupValue = value;
+                              });
 
-                            widget.onChanged(value);
-                          }
-                        },
-                        controlAffinity: ListTileControlAffinity.trailing,
+                              widget.onChanged(value);
+                            }
+                          },
+                          controlAffinity: ListTileControlAffinity.trailing,
+                        ),
                       ),
                     )
                   ],
